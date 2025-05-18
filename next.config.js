93e1f9edaaac8c991f93a6a7f0f4c8ba1fda56e5
diva-sl <divakaran.sivasamy@gmail.com>
2025-05-18 18:29:03 +0530

    jshjdshjdshdsj

--- next.config.js
+++ next.config.js
@@ -8,35 +8,6 @@ const nextConfig = {
   },
   trailingSlash: true,
 
-  async redirects() {
-    return [
-      // Redirect non-www to www
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "algotradingelite.com",
-          },
-        ],
-        destination: "https://www.algotradingelite.com/:path*",
-        permanent: true,
-      },
-
-      // Or redirect www to non-www (choose one block to keep)
-      // {
-      //   source: '/:path*',
-      //   has: [
-      //     {
-      //       type: 'host',
-      //       value: 'www.algotradingelite.com',
-      //     },
-      //   ],
-      //   destination: 'https://algotradingelite.com/:path*',
-      //   permanent: true,
-      // },
-    ];
-  },
 };
 
 module.exports = nextConfig;
--- utils/getBackendUrl.js
+++ utils/getBackendUrl.js
@@ -4,9 +4,9 @@ export const getBackendUrl = () => {
   // Check if the app is running on localhost or on a remote server
   if (window.location.hostname === "localhost") {
     // Local environment, use localhost
-    return "http://localhost:5000";
+    return "http://localhost:5000/api";
   } else {
     // Production environment, use your production server
-    return "https://97.74.87.147"; // Replace with your production URL
+    return "https://97.74.87.147/api"; // Replace with your production URL
   }
 };
